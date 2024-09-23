ab1317520341718f250872f1132972e36c11b46e
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-09-23 12:59:25 +0500

    Initial Commit

---
+++ FortuneTellingAndMemes/FortuneTellingAndMemesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortuneTellingAndMemesApp.swift
+//  FortuneTellingAndMemes
+//
+//  Created by Даниил Сивожелезов on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FortuneTellingAndMemesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
