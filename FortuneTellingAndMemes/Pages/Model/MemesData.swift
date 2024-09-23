c2754993a6bfe472bf8fb76ce4905f2393260ce9
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-09-23 19:39:11 +0500

    create request to api and configure ui for display image

---
+++ FortuneTellingAndMemes/Pages/Model/MemesData.swift
@@ -0,0 +1,13 @@
+//
+//  MemesData.swift
+//  FortuneTellingAndMemes
+//
+//  Created by Даниил Сивожелезов on 23.09.2024.
+//
+
+import Foundation
+
+struct MemesData: Decodable {
+    let success: Bool
+    let data: Memes
+}
